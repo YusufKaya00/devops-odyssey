9aa978f081bc46b7c5207696f4c647a05ff4251c
ysf <inanyusufkaya12@gmail.com>
2026-09-24 15:48:57 +0300

    launcing website

--- server.js
+++ server.js
@@ -406,4 +406,9 @@ async function startServer() {
   });
 }
 
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 startServer();
